625474bd2814233890afc23e93f8b9a4c4b94fe8
Timespike <50344633+Timespike@users.noreply.github.com>
2021-12-09 19:58:09 -0600

    updated with quote API function

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log('testing');
+let apiQuotes = [];
+
+// Show New QUote
+function newQuote() {
+    // Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
+// Get Quotes From API
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+      const response = await fetch(apiUrl);
+      apiQuotes = await response.json();
+      newQuote();
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+// On Load
+getQuotes();
